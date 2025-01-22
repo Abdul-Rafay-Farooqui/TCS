f0f69bcc119771f86102652e0172b17d19479cd0
Abdul-Rafay-Farooqui <farooquiabdul85@gmail.com>
2025-01-22 13:48:26 +0500

    aligned button

--- app/components/case-study/Media.js
+++ app/components/case-study/Media.js
@@ -27,27 +27,27 @@ const Media = ({ type, url, style }) => {
           ></video>
           <div
             onClick={toggleMute}
-            className="absolute bottom-[24px] z-50 right-[24px] flex items-center justify-center bg-tcs-banana group hover:bg-tcs-pure-black size-8 cursor-none r-transition  rounded-[50%] "
+            className="absolute bottom-[12px] sm:bottom-[24px] z-50 right-[12px] sm:right-[24px] flex items-center justify-center bg-tcs-banana group sm:hover:bg-tcs-pure-black size-[24px] sm:size-8 cursor-none r-transition  rounded-[50%] "
           >
             {isMuted ? (
               <span className="pl-0.5" role="img" aria-label="Muted">
                 <img
-                  className="size-5 block group-hover:hidden"
+                  className="size-4 sm:size-5 block sm:group-hover:hidden"
                   src="/image/icon/silent.png"
                 />
                 <img
-                  className="size-5 hidden group-hover:block"
+                  className="size-4 sm:size-5 hidden sm:group-hover:block"
                   src="/image/icon/silent-y.png"
                 />
               </span>
             ) : (
               <span className="pl-0.5" role="img" aria-label="Unmuted">
                 <img
-                  className="size-5 group-hover:hidden"
+                  className="size-4 sm:size-5 sm:group-hover:hidden"
                   src="/image/icon/volume.png"
                 />
                 <img
-                  className="size-5 hidden group-hover:block"
+                  className="size-4 sm:size-5 hidden sm:group-hover:block"
                   src="/image/icon/volume-y.png"
                 />
               </span>
